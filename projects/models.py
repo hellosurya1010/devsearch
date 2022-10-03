828cb0c88cae96f1b18b5b257fd44e22f996ffca
hellosurya1010 <ayrus4444@gmail.com>
2022-10-03 09:38:09 +0530

    first commit

---
+++ projects/models.py
@@ -0,0 +1,48 @@
+from operator import mod
+from statistics import mode
+from wsgiref.simple_server import demo_app
+from django.db import models
+import uuid
+
+# Create your models here.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    demo_link = models.CharField(max_length=2000, null=True, blank=True)
+    featured_image = models.ImageField(null=True, blank=True, default="resibee.png")
+    source_link = models.CharField(max_length=2000, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_totel = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    VOTE_TYPE = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+
+    def __str__(self):
+        return self.value
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+
---
+++ projects/views.py
@@ -0,0 +1,47 @@
+from multiprocessing import context
+from multiprocessing.dummy import current_process
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
+
+projectlist = [
+    {'id': 1, 'name': 'ehealth', 'description': 'Medical app'},
+    {'id': 2, 'name': 'ndgeme', 'description': 'Financial app'},
+    {'id': 3, 'name': 'letters pack', 'description': 'Chatting app'},
+]
+
+def projects(request):
+    projects = Project.objects.all()
+    return render(request, 'projects/index.html', {'projects': projects})
+
+def project(request, id):
+    project = Project.objects.get(id=id)
+    tags = project.tags.all()
+    return render(request, 'projects/show.html', {'project': project, 'tags': tags })
+
+def createProject(request):
+    forms = ProjectForm()
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'forms': forms}
+    return render(request, 'projects/form.html', context)
+
+def updateProject(request, id):
+    project = Project.objects.get(id=id)
+    forms = ProjectForm(instance=project)
+    if request.method == "POST":
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'forms': forms}
+    return render(request, 'projects/form.html', context)
+
+def deleteProject(request, id):
+    project = Project.objects.get(id=id)
+    project.delete()
+    return redirect('projects') 
